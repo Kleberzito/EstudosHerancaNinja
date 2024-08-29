6fbcb0b6143a36a4bf6442b1c239ac4d69624777
Kleber Gomes <kleber.gsilva@outlook.com>
2024-08-29 11:04:58 -0300

    Criado a classe Ninja

---
+++ Ninja.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Ninja
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Village { get; set; }
+
+    public Ninja(string name, int age, string village)
+    {
+        Name = name;
+        Age = age;
+        Village = village;
+    }
+
+    public override string ToString()
+    {
+        return $"Name:    {Name}\nAge:     {Age}\nVillage: {Village}\n";
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ninja Naruto = new Ninja("Naruto", 17, "Vila da folha");
+        Ninja Sasuke = new Ninja("Sasuke", 18, "Vila da folha");
+        Ninja Sakura = new Ninja("Sakura", 18, "Vila da folha");
+        Ninja Hinata = new Ninja("Hinata", 16, "Vila da folha");
+
+        Console.WriteLine(Naruto.ToString());
+        Console.WriteLine(Sasuke.ToString());                     
+        Console.WriteLine(Sakura.ToString());
+        Console.WriteLine(Hinata.ToString());
+    }
+    
+}
