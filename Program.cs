2000fea8fe668a4734ef372f210ef6d16946dad4
Kleber Gomes <kleber.gsilva@outlook.com>
2024-08-29 12:55:49 -0300

    Adiciona nova classe derivada Uchira com método PowerCla

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Uzumaki Naruto = new Uzumaki("Naruto", 17, "Vila da folha", false);        
-        Ninja Sasuke = new Ninja("Sasuke", 18, "Vila da folha");
+        Uzumaki Naruto = new Uzumaki("Naruto", 17, "Vila da folha", true);        
+        Uchira Sasuke = new Uchira("Sasuke", 18, "Vila da folha", true);
         Ninja Sakura = new Ninja("Sakura", 18, "Vila da folha");
         Ninja Hinata = new Ninja("Hinata", 16, "Vila da folha");
 
---
+++ Uchira.cs
@@ -0,0 +1,20 @@
+public class Uchira : Ninja
+{
+    public string PowerClaStatus { get; private set; }
+
+    public Uchira(string name, int age, string village, bool powerCla) : base(name, age, village)
+    {
+        PowerClaStatus = PowerCla(powerCla);        
+    }
+    private string PowerCla(bool status)
+    {
+        if(status)
+            return "Ativado Sharingan.";
+        return "Não usou o Sharingan";
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $"{base.Name}: {PowerClaStatus}\n";
+    }
+}
